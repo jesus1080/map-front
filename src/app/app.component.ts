ef45f4c219a7b869d031f6a4386c381b5a348eaa
Jesus Ruiz <jesusruiz@unicauca.edu.co>
2024-07-02 08:18:18 -0500

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MapComponent } from './map/map.component';
+import { FormsModule } from '@angular/forms';
+import { MarkerFormComponent } from './marker-form/marker-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MapComponent, FormsModule, MarkerFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
